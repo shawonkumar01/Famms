3e55153ec1e6efe2cd0e9bd4cf77501c0b34faba
Shawon Kumar Modak <89266007+shawonkumar01@users.noreply.github.com>
2025-07-13 23:05:39 +0600

    admin dashboard template

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,46 +1,71 @@
 <header class="header_section">
-            <div class="container">
-               <nav class="navbar navbar-expand-lg custom_nav-container ">
-                  <a class="navbar-brand" href="/"><img width="250" src="images/logo.png" alt="#" /></a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class=""> </span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                     <ul class="navbar-nav">
-                        <li class="nav-item active">
-                           <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                       <li class="nav-item dropdown">
-                           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Pages <span class="caret"></span></a>
-                           <ul class="dropdown-menu">
-                              <li><a href="about.html">About</a></li>
-                              <li><a href="testimonial.html">Testimonial</a></li>
-                           </ul>
-                        </li>
-                        <li class="nav-item">
-                           <a class="nav-link" href="product.html">Products</a>
-                        </li>
-                        <li class="nav-item">
-                           <a class="nav-link" href="blog_list.html">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                           <a class="nav-link" href="contact.html">Contact</a>
-                        </li>
-                        <form class="form-inline">
-                           <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                           <i class="fa fa-search" aria-hidden="true"></i>
-                           </button>
-                        </form>
-                         <li class="nav-item">
-                           <a class="btn btn-primary" id="logincss"href="{{ route('login') }}">Login</a>
-                        </li>
-                        </li>
-                         <li class="nav-item">
-                           <a class="btn btn-success" href="{{ route('register') }}">Register</a>
-                        </li>
-                        
-                     </ul>
-                  </div>
-               </nav>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+            <a class="navbar-brand" href="/"><img width="250" src="images/logo.png" alt="#" /></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class=""> </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button"
+                            aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Pages <span
+                                    class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="testimonial.html">Testimonial</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="product.html">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="blog_list.html">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Contact</a>
+                    </li>
+                    <form class="form-inline">
+                        <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                    @if (Route::has('login'))
+                        @auth
+                            <div class="dropdown ml-3">
+                                <!-- Trigger Button -->
+                                <a class="dropdown-toggle user-menu" href="#" role="button" data-toggle="dropdown">
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <!-- Dropdown Menu -->
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <!-- Profile Link -->
+                                    <a class="dropdown-item" href="{{ route('profile.edit') }}">
+                                        <i class="fas fa-user mr-2"></i> Profile
+                                    </a>
+
+                                    <!-- Logout Form -->
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); this.closest('form').submit();">
+                                            <i class="fas fa-sign-out-alt mr-2"></i> Log Out
+                                        </a>
+                                    </form>
+                                </div>
+                            </div>
+                        @else
+                            <a href="{{ route('login') }}"id="logincss"class="btn btn-primary">Login</a>
+                            <a href="{{ route('register') }}" class="btn btn-success">Register</a>
+                        @endauth
+                    @endif
+                </ul>
             </div>
-         </header>
+        </nav>
+    </div>
+</header>
