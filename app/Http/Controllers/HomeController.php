d87931113dabb53f3984babde03fc784ffa7d545
Shawon Kumar Modak <89266007+shawonkumar01@users.noreply.github.com>
2025-07-16 14:37:36 +0600

    cart data added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\Cart;
 class HomeController extends Controller
 {
     public function index()
@@ -18,7 +18,7 @@ class HomeController extends Controller
         $product = Product::find($id);
         return view("home.product_details", compact("product"));
     }
-    public function add_cart($id)
+    public function add_cart(Request $request, $id)
     {
         if (!Auth::check()) {
             return redirect("login");
@@ -27,9 +27,24 @@ class HomeController extends Controller
         // Proceed with adding to cart
         $user = Auth::user();
         $product = Product::find($id); // assuming your model is Product
-      
+        
+        $cart = new Cart();
+        $cart->name = $user->name;
+        $cart->email = $user->email;
+        $cart->phone = $user->phone;
+        $cart->address = $user->address;
+        $cart->user_id = $user->id;
+        $cart->product_title = $product->title;
+        $cart->price = $product->price;
+        $cart->image = $product->image;
+        $cart->product_id = $product->id;
+        $cart->quantity = $request->quantity;
 
-         return redirect()->route("home.userpage")->with('success', 'Product added to cart!');
+        $cart->save();
+
+
+
+        return redirect()->route("home.userpage")->with('success', 'Product added to cart!');
 
     }
 }
