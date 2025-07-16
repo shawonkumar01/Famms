701c6a82bd371e8b5ab9d7edce305401a064945d
Shawon Kumar Modak <89266007+shawonkumar01@users.noreply.github.com>
2025-07-16 10:58:49 +0600

    pruduct fetch from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view("home.userpage");
+        $products = Product::paginate(9);
+        return view("home.userpage", compact("products"));
     }
 }
